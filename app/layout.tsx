96c20c8b4bf21626a1f773c9323e09132dfa89e0
michaelsipper <sippermichael@gmail.com>
2024-11-09 21:21:44 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
